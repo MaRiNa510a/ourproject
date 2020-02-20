1f1b74dfdca9b274b00dcce01a1d804e83c16c40
marina <5m.clover3@gmail.com>
2020-02-20 13:58:00 +0100

    day13

--- envs/env_Q.py
+++ envs/env_Q.py
@@ -11,55 +11,61 @@ LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 EPISODES = 2000
 
-image_in = cv2.imread("smile_test.png")           #import image           (form of every pixel  [255, 255, 255] or [0, 0, 0]
+#import image           (form of every pixel  [255, 255, 255] or [0, 0, 0]
+image_in = cv2.imread("smile_test.png")
+# declaration  output image
+image_out = np.ones(image_in.shape, np.uint8) * 255
+#declaration input array to have the image in another form (easier to work with)     black->1   white->0
+image_in_array = np.zeros(image_in.shape)
+#declaration output array to have the image in another form (easier to change the value of pixels)     black->1   white->0
+image_out_array = np.zeros(image_in.shape)
 
-image_out = np.ones(image_in.shape, np.uint8) * 255 #declaration  output image
-image_in_array = np.zeros(image_in.shape)                   #declaration input array to have the image in another form (easier to work with)     black->1   white->0
-image_out_array = np.zeros(image_in.shape)                  #declaration output array to have the image in another form (easier to change the value of pixels)     black->1   white->0
-
-
-
+#make class of environment
 class FooEnv(gym.Env):
 
-
+  #set the initial state
   def __init__(self, env):
     self.env = env
-
     self.action_space = self.env.action_space
-
     self.observation_space = self.env.observation_space
-
     self.reward_range = self.env.reward_range
-
     self.metadata = self.env.metadata
 
-  def step(self, action):           # actionを実行し、結果を返す
+  #pursue actions and return the result
+  def step(self, action):
       for y_pixel in range(0, image_in.shape[1]):
           for x_pixel in range(0, image_in.shape[0]):
               if all(image_in[x_pixel, y_pixel]) == True:
-                  image_in_array[
-                      x_pixel, y_pixel] = 0  # transform image to array           1 means black   0 means white
+                  # transform image to array           1 means black   0 means white
+                  image_in_array[x_pixel, y_pixel] = 0
               else:
                   image_in_array[x_pixel, y_pixel] = 1
+
       x_pixel = random.randrange(0, image_in_array.shape[0], 1)  # last 1 is step1
       print("x = " + str(x_pixel))
       y_pixel = random.randrange(0, image_in_array.shape[1], 1)
       print("y = " + str(y_pixel))
       print("\n")
+
       plt.plot(x_pixel, y_pixel, color="red", marker="o", linestyle="None")
+
       which_action = random.randrange(1, 5, 1)  # 1 straight, 2 right, 3 back, 4 left
+
       action_position = []
       for i in range(0, 1000):
           action_position += str(which_action)
+
           with open('action3.csv', 'w') as file:
               writer = csv.writer(file, lineterminator='\n')
               writer.writerows(action_position)
           print(which_action)
+
           matrix_3x3 = [
               [image_in_array[x_pixel - 1][y_pixel - 1], image_in_array[x_pixel][y_pixel - 1], image_in_array[x_pixel + 1][y_pixel - 1]],
               [image_in_array[x_pixel - 1][y_pixel], image_in_array[x_pixel][y_pixel], image_in_array[x_pixel + 1][y_pixel]],
               [image_in_array[x_pixel - 1][y_pixel + 1], image_in_array[x_pixel][y_pixel + 1], image_in_array[x_pixel + 1][y_pixel + 1]]
                         ]
+
           image_out_array[x_pixel][y_pixel] = 1
           x1 = np.argmax(matrix_3x3)
           if x1 == 1:
@@ -90,12 +96,15 @@ class FooEnv(gym.Env):
               x_pixel = x_pixel - 1
 
       position = []
-      for y_pixel in range(0, image_out_array.shape[1]):  # transform array to image       1->[0, 0, 0]    0->[255, 255, 255]
+      # transform array to image       1->[0, 0, 0]    0->[255, 255, 255]
+      for y_pixel in range(0, image_out_array.shape[1]):
           for x_pixel in range(0, image_out_array.shape[0]):
-              if all(image_out_array[x_pixel, y_pixel]) == True:  # if [x_pixel]==0 or [y_pixel]==0 -> false
+              # if [x_pixel]==0 or [y_pixel]==0 -> false
+              if all(image_out_array[x_pixel, y_pixel]) == True:
                   image_out[x_pixel, y_pixel] = [0, 0, 0]
                   position += [[x_pixel, y_pixel]]
                   # print(position[-1])
+
                   with open('xy3.csv', 'w') as file:
                       writer = csv.writer(file, lineterminator='\n')
                       writer.writerows(position)
@@ -103,31 +112,34 @@ class FooEnv(gym.Env):
                   image_out[x_pixel, y_pixel] = [255, 255, 255]
 
       cv2.imshow("output image", image_out)  # displaying out image
+
       position_plot = []
+      #count the x,y position
       idx = position.index(position[-1])
       print(idx)
+      #pick the position every 10 points
       a = int((idx + 1) / 10)
       for i in range(1, a):
           position_plot += [position[i * 10 - 1]]
       print(position_plot)
       plt.plot(position_plot, color="black", marker="o", linestyle="None")
       plt.show()
+
       return self.env.step(action)
 
 
-
-  def reset(self):                  # 状態を初期化し、初期の観測値を返す
-
-
+  #reset the state and return the initial observation
+  def reset(self):
       return self.env.reset
 
-  def render(self, close=False): # 環境を可視化する
+  #make the environment visible
+  def render(self, close=False):
       return self.env.render
 
-  def close(self): # 環境を閉じて後処理をする
-
-
+  #close the environment and perform post-procesing
+  def close(self):
       return self.env.close()
 
+  #lock Random Seed
   def seed(self, seed=None): # ランダムシードを固定する
       return self.env.seed(seed)
